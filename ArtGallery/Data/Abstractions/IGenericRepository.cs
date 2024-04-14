e86de49b251a1727f31a9380468bb0c0254fd33a
Dmitriy Parkhomenko <dparhomenko84@gmail.com>
2024-04-14 10:21:25 +0300

    feat(repository): added interfaces for generic repository and unit of work

---
+++ ArtGallery/Data/Abstractions/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace ArtGallery.Data.Abstractions;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    public Task<IEnumerable<TEntity>> GetAsync(
+        Expression<Func<TEntity, bool>>? filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null);
+
+    public Task<TEntity?> GetByIdAsync(object id);
+
+    public Task CreateAsync(TEntity entity);
+
+    public Task DeleteAsync(object id);
+
+    public Task DeleteAsync(TEntity entityToDelete);
+
+    public Task UpdateAsync(TEntity entityToUpdate);
+}
---
+++ ArtGallery/Data/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ArtGallery.Data.Implementations;
+using ArtGallery.Models.Structs.Entity;
+
+namespace ArtGallery.Data.Abstractions;
+
+public interface IUnitOfWork
+{
+    public IGenericRepository<ApplicationUser> UserRepository { get; }
+
+    public IGenericRepository<Art> ArtRepository { get; }
+
+    public IGenericRepository<Artist> ArtistRepository { get; }
+
+    public IGenericRepository<Genre> GenresRepository { get; }
+
+    public Task SaveAsync();
+}
